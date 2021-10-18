591181bf2ee9877316f23d723885c3f9d63bc56c
hasan-naim <hasannaim6246@gmail.com>
2021-10-18 18:37:45 +0600

    lesson 11 is done

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { AuthProvider } from "../context/AuthContext";
 import "../styles/App.css";
 import Layout from "./Layout";
+import PublicRoute from "./PablicRoute";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 import Signup from "./pages/Signup";
+import PrivetRoute from "./PrivetRoute";
 
 function App() {
   return (
@@ -15,10 +17,10 @@ function App() {
         <Layout>
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/quiz" component={Quiz} />
-            <Route exact path="/result" component={Result} />
+            <PublicRoute exact path="/signup" component={Signup} />
+            <PublicRoute exact path="/login" component={Login} />
+            <PrivetRoute exact path="/quiz" component={Quiz} />
+            <PrivetRoute exact path="/result" component={Result} />
           </Switch>
         </Layout>
       </AuthProvider>
