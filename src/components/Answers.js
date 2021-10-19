819c485770ad0cb4610e59b541cd7dd1ec563486
hasan-naim <hasannaim6246@gmail.com>
2021-10-19 18:33:07 +0600

    lesson 14 is done result page is done

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -1,17 +1,36 @@
+import { Fragment } from "react/cjs/react.production.min";
 import classes from "../styles/Answers.module.css";
 import Checkbox from "./Checkbox";
 
-export default function Answers({ options = [], handleChange }) {
+export default function Answers({ options = [], handleChange, input }) {
+  console.log("this is from answers", options);
   return (
     <div className={classes.answers}>
       {options.map((option, index) => (
-        <Checkbox
-          className={classes.answer}
-          text={option.title}
-          value={index}
-          checked={option.checked}
-          onChange={(e) => handleChange(e, index)}
-        />
+        <Fragment key={index}>
+          {input ? (
+            <Checkbox
+              className={classes.answer}
+              text={option.title}
+              value={index}
+              checked={option.checked}
+              onChange={(e) => handleChange(e, index)}
+            />
+          ) : (
+            <Checkbox
+              className={`${classes.answer} ${
+                option.correct
+                  ? classes.correct
+                  : option.checked
+                  ? classes.wrong
+                  : null
+              }`}
+              text={option.title}
+              defaultChecked={option.checked}
+              disabled
+            />
+          )}
+        </Fragment>
       ))}
     </div>
   );
--- src/components/pages/Result.js
+++ src/components/pages/Result.js
@@ -1,11 +1,53 @@
+import _ from "lodash";
+import { useHistory, useParams } from "react-router";
+import useAnswers from "../../hooks/useAnswers";
 import Analysis from "../Analysis";
 import Summary from "../Summary";
 
 export default function Result() {
+  const { id } = useParams();
+  const { location } = useHistory();
+  const { state } = location;
+  const { qna } = state;
+
+  const { loading, error, answers } = useAnswers(id);
+  console.log(answers);
+
+  function calculate() {
+    let score = 0;
+
+    answers.forEach((question, index1) => {
+      let correctIndex = [];
+      let checkedIndexes = [];
+
+      question.options.forEach((option, index2) => {
+        if (option.correct) correctIndex.push(index2);
+        if (qna[index1].options[index2].checked) {
+          checkedIndexes.push(index2);
+          option.checked = true;
+        }
+      });
+      if (_.isEqual(correctIndex, checkedIndexes)) {
+        score = score + 5;
+      }
+    });
+
+    return score;
+  }
+
+  const userScore = calculate();
+
   return (
     <>
-      <Summary />
-      <Analysis />
+      {loading && <div>Loading...</div>}
+      {error && <div>There was an error. Please try agian!</div>}
+
+      {answers && answers.length > 0 && (
+        <>
+          <Summary score={userScore} noq={answers.length} />
+          <Analysis answers={answers} />
+        </>
+      )}
     </>
   );
 }
